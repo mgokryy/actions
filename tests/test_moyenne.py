b66c0ed994e3d24e716f34d6f2e2412c025ec6f7
mgokryy <omonlola-yabo-marie-grace.okry@efrei.net>
2026-01-13 15:50:45 +0100

    verr

--- tests/test_moyenne.py
+++ tests/test_moyenne.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from moyenne import moyenne
+
+
 
 def test_moyenne_simple():
     assert moyenne([2, 4, 6]) == 4
